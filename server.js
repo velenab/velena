fb5feb61ad33e0b059d16adb8afeda7928d80e66
velenab <velena.batchenko@gmail.com>
2026-06-03 11:07:39 +0000

    frontend

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const PORT = 3000;
+
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+}
+
+app.use(express.static("public"));
+
+httpServer.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:$(PORT)`)
+});
